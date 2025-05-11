4335b91a847bf000eb031242c16b9954dda060a1
anujayavidmal2002 <vidmalssaa.22@uom.lk>
2025-05-12 02:14:02 +0530

    67min commit

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,18 +1,35 @@
-import React from "react";
-import Navbar from "@/app/(components)/Navbar"
-import Sidebar from "@/app/(components)/Sidebar";
+"use client";
 
-const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+import React from "react";
+import Navbar from "@/app/(components)/Navbar";
+import Sidebar from "@/app/(components)/Sidebar";
+import StoreProvider, { useAppSelector } from "./redux";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+
+ 
+
   return (
     <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
-      <Sidebar/>
+      <Sidebar />
       <main
-        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-200 md:pl-24`}
-      > 
-      <Navbar/>
+        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-200 ${isSidebarCollapsed ? "md:pl-24" : "md:pl-72"}`}
+      >
+        <Navbar />
         {children}
       </main>
     </div>
+  );
+};
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   );
 };
 
