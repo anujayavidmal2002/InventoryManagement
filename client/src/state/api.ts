b4957c9f9c9a5b3a8d30ef8c2201e5c8b287fa8e
anujayavidmal2002 <vidmalssaa.22@uom.lk>
2025-05-12 14:46:52 +0530

    140min commit

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -4,7 +4,12 @@ export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   reducerPath: "api",
   tagTypes: [],
-  endpoints: (build) => ({}),
+  endpoints: (build) => ({
+    getDashboardMetrics: build.query<DashboardMetrics, void>({
+      query: () => "/dashboard"
+      provideTage: ["SashboardMetrics"]
+    }),
+  }),
 });
 
 export const {} = api;
--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -9,8 +9,12 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
-/*  Route imports */
-/* configurations */
+/* ROUTE IMPORTS */
+const dashboardRoutes_1 = __importDefault(require("./routes/dashboardRoutes"));
+//import productRoutes from "./routes/productRoutes";
+//import userRoutes from "./routes/userRoutes";
+//import expenseRoutes from "./routes/expenseRoutes";
+/* CONFIGURATIONS */
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
@@ -20,12 +24,13 @@ app.use((0, morgan_1.default)("common"));
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.use((0, cors_1.default)());
-/* Routes */
-app.get("/hello", (req, res) => {
-    res.send("hello world");
-});
-/* Server */
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-    console.log(`Server  running on port ${port}`);
+/* ROUTES */
+app.use("/dashboard", dashboardRoutes_1.default); // http://localhost:8000/dashboard
+//app.use("/products", productRoutes); // http://localhost:8000/products
+//app.use("/users", userRoutes); // http://localhost:8000/users
+//app.use("/expenses", expenseRoutes); // http://localhost:8000/expenses
+/* SERVER */
+const port = Number(process.env.PORT) || 3001;
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server running on port ${port}`);
 });
